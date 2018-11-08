2881c0745449530a1a3d002444fe51f09dbc4ccf
xIS12719 <alvaro.tellezrodriguez@extern.isban.com>
2018-11-08 12:17:02 +0100

    Exported the set and update methods

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const logHandler = require("./log4nr/logHandler");
+const levelsConfiguration = require("./log4nr/levelsConfiguration");
+
 module.exports = {
-  "logHandler": require("./log4nr/logHandler")
+  "logHandler": logHandler,
+  "setLoggingConfiguration": levelsConfiguration.set,
+  "updateLoggingConfiguration": levelsConfiguration.update
 }

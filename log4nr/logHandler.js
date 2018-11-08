5239fc7539c136257779b0f462e77ad6bcc8e59a
xIS12719 <alvaro.tellezrodriguez@extern.isban.com>
2018-11-08 12:25:43 +0100

    Small not important changes

--- log4nr/logHandler.js
+++ log4nr/logHandler.js
@@ -1,14 +1,19 @@
 const connector = require("./connector");
 
+/**
+ * It analyzes the log events and when the "runtime event" called "runtime-deploy" happens
+ * it'll call the connected function that will take care of the deployments
+ * 
+ * @param {Object} settings The logging configuration
+ */
 function logAnalyzer(settings) {
   return function(msg) {
-    //console.log(`mi log handler: ${msg.level}\n${JSON.stringify(msg)}`);
-    //console.log(`${JSON.stringify(msg)}`);
+    // LEVEL 60=TRACE
     if (msg.level === 60 && msg.msg.startsWith("runtime event")) {
       //console.log(msg.msg);
       const evt = JSON.parse(msg.msg.replace("runtime event: ", ""));
       if (evt.id === "runtime-deploy") {
-        console.log("A deploy has been done!");
+        console.log("A flows deployment has been done!");
         connector.execDeployFunction();
       }
     }
@@ -16,6 +21,8 @@ function logAnalyzer(settings) {
 }
 
 
+// the exported object is the one to be set in the node-red's settings.js
+// it includes all the attributes that it has to have
 module.exports = {
   "level": "trace",
   "metrics": false,

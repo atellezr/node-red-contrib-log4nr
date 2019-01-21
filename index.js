1a75c84a35e7b667bb9f578effa10dd6447c9e38
Álvaro Téllez <atellezro@gmail.com>
2019-01-21 14:54:46 +0100

    Update index.js
    Exported also a method for retrieving the configuration

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const levelsConfiguration = require("./log4nr/levelsConfiguration");
 
 module.exports = {
   "logHandler": logHandler,
+  "getConfig": function(){return levelsConfiguration.config},
   "setLoggingConfiguration": levelsConfiguration.set,
   "updateLoggingConfiguration": levelsConfiguration.update
 }
